7353332fa31615bf3162ef6f468c1ed12c73e403
Dhananjay M.R <dhananjaymr@yahoo.com>
2023-09-27 21:05:50 +0530

    Minor changes to error handling done

--- Basic Connection Establishment Program/2_Connection Establishment with error handling.py
+++ Basic Connection Establishment Program/2_Connection Establishment with error handling.py
@@ -1,8 +1,18 @@
 import mysql.connector
+
 try:
-    x = mysql.connector.connect(host='localhost', user='root', password='admin.123')
+    x = mysql.connector.connect(host='locahost', user='root', password='admin.123')
 except mysql.connector.Error:
     print('Sorry Check your Database Host User ID/Password!')
-if x.is_connected():
-    db_info = x.get_server_info()
-    print('Connection to MySQL', db_info, 'established successfully!')
+
+try:
+    if x.is_connected():
+        db_info = x.get_server_info()
+        print('Connection to MySQL', db_info, 'established successfully!')
+except NameError:
+    print('Program Terminated')
+
+finally:
+    print('Thanks for using the program')
+    print('»»——————---——————-««')
+    print('Source Code by: Dhananjay M.R')
