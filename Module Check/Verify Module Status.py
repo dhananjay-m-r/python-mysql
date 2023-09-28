d576172c55d309987f4ee251bbf41a158ad45155
Dhananjay M.R <dhananjaymr@yahoo.com>
2023-09-28 14:05:03 +0530

    Source Code for the program created
    The Source code for this program checks and lets you know whether the required module for MySQL python connectivity is installed on your device.
    
    If the module is not installed kindly install the module
    
    There are various videos on youtube which gives you step by step procedure to install the required module to establish connection between MySQL and python

---
+++ Module Check/Verify Module Status.py
@@ -0,0 +1,12 @@
+try:
+    import mysql.connector
+    print('Required module is already installed!')
+
+except ImportError:
+    print('Module Not Found, Kindly Install the required module')
+
+finally:
+    print('')
+    print('Thank you for using the program')
+    print('»»——————---——————-««')
+    print('Source Code: Dhananjay M.R')
