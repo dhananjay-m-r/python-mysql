31d77f993950f3ce0aa73a0415f3545a5d204e05
Dhananjay M.R <dhananjaymr@yahoo.com>
2023-09-28 15:21:21 +0530

    Source Code for the program created
    The source code for the program allows the user to create and view the list of available databases on the server.
    
    Note: The server details such as Host ID, User ID & Password has to be given by the user as input during runtime.
    
    If you are facing issues with MySQL + Python connectivity, kindly go through the use the appropriate diagnostic program whose source code is available in the main branch of this repository in its respective folder.

---
+++ Create & Drop/Databases/Create and view list of existing database.py
@@ -0,0 +1,43 @@
+m = input('Enter the database name which you would like to create: ')
+n = 'create database ' + m + ';'
+
+print('')
+print('Kindly enter your database details below to create the database', m)
+print('')
+
+a = input('Enter your host ID: ')
+b = input('Enter your MySQL User ID: ')
+c = input('Enter the password for the mentioned MySQL User: ')
+
+import mysql.connector
+x = mysql.connector.connect(host = a, user = b, password = c)
+y = x.cursor()
+y.execute(n)
+
+print('Database Successfully Created!')
+print('')
+print('»»——————---——————-««')
+
+View = input('Would you like to view the list of all the databases present on your server? (Y/N): ')
+print('')
+Yes = 'Yy'
+
+if View in Yes:
+    c = 0
+    y = x.cursor()
+    databases = ("show databases")
+    y.execute(databases)
+    print('»»——————---——————-««')
+    print('The list of database present on your server is as below: ')
+    for (databases) in y:
+        print('-> ', databases[0])
+        c =+ 1
+    print('')
+    print('Total no of database detected on your server is: ', c)
+
+x.close()
+
+print('')
+print('Thank you for using the program')
+print('»»——————---——————-««')
+print('Source Code: Dhananjay M.R')
