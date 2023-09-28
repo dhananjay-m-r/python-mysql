d3babd7b57ee36b4a8ca7e63632c1662bc3e1a72
Dhananjay M.R <dhananjaymr@yahoo.com>
2023-09-28 18:08:24 +0530

    Source Code for the program created
    The source code for the program allows the user to view the list of available databases on the server & drop them.
    The source code comes with the loop function which the user can terminate until the required number of databases are deleted.
    
    Note: The server details such as Host ID, User ID & Password has to be given by the user as input during runtime.
    
    If you are facing issues with MySQL + Python connectivity, kindly go through the use the appropriate diagnostic program whose source code is available in the main branch of this repository in its respective folder.
    
    Disclaimer: Once a database is dropped, it cannot be retrieved back.

---
+++ Create & Drop/Databases/Drop Databases with loop.py
@@ -0,0 +1,100 @@
+print('Before we get started, Kindly enter your database details below: ')
+print('')
+
+a = input('Enter your host ID: ')
+b = input('Enter your MySQL User ID: ')
+c = input('Enter the password for the mentioned MySQL User: ')
+
+import mysql.connector
+x = mysql.connector.connect(host = a, user = b, password = c)
+y = x.cursor()
+
+print('')
+print('»»——————---——————-««')
+print('')
+
+c = 0
+databases = ("show databases")
+y.execute(databases)
+
+print('')
+print('»»——————---——————-««')
+print('')
+print('Here below is the list of database present on your server: ')
+print('')
+
+for (databases) in y:
+    print('-> ', databases[0])
+    c += 1
+
+print('')
+print('Total no of database detected on your server is: ', c)
+
+print('')
+print('»»——————---——————-««')
+print('')
+
+print('Dropping a database means you will loose the data stores in it as well')
+
+def DropDatabase():
+    m = input('Enter the database name which you would like to drop/delete: ')
+    n = 'drop database ' + m + ';'
+
+    print('')
+
+    try:
+        y.execute(n)
+        print('The database ', m, ' dropped successfully!')
+        print('')
+        
+        ShowRemaining = input('Do you want to view the updated list of databases on your server (Y/N): ')
+
+        d = 0
+
+        if ShowRemaining in Yes:
+            databases = ("show databases")
+            y.execute(databases)
+
+            print('')
+            print('»»——————---——————-««')
+            print('')
+            print('Here below is the updated list of database present on your server: ')
+            print('')
+
+            for (databases) in y:
+                print('-> ', databases[0])
+                d += 1
+            print('')
+            print('Currently you have', d, 'databases on your server')
+
+            print('')
+            print('»»——————---——————-««')
+            print('')
+
+    except mysql.connector.Error:
+        print('Error Encountered! Database doesnt exist!!')
+        print('Enter a database name that exists!!')
+
+print('')
+confirmation = input('Would you like to drop a table (Y/N): ')
+Yes = 'Yy'
+
+while confirmation in Yes:
+    DropDatabase()
+    LoopAgain = input('Do you want to drop more databases?(Y/N): ')
+    print('')
+    if LoopAgain in Yes:
+        DropDatabase()
+    else:
+        break
+
+print('Program Terminated!')
+
+print('')
+x.close()
+print('MySQL Python Connection Closed!')
+
+print('')
+print('Thank you for using the program')
+print('»»——————---——————-««')
+print('Source Code by: Dhananjay M.R')
